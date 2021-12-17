51448b9bc4b1005604d112212a3fc3fb40a7573e
AdailtonSimplicio <adailtonsimplicio.s@gmail.com>
2021-12-16 22:33:13 -0300

    Trabalhando com Profiles e Configuraçoes

---
+++ Bootcamps/01 - TQI Java Developer/Introdução ao framework Spring Boot/springbootconfig/src/test/java/com/digitalinovationone/springbootconfig/SpringbootconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinovationone.springbootconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
