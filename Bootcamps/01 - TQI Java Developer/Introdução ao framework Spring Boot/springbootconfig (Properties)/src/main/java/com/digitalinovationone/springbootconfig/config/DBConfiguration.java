4cd0fbaa694ac752feca96d2a26b76e4d4dbaff2
AdailtonSimplicio <adailtonsimplicio.s@gmail.com>
2021-12-18 22:26:25 -0300

    Trabalhando com Profiles e Configurações

---
+++ Bootcamps/01 - TQI Java Developer/Introdução ao framework Spring Boot/springbootconfig (Properties)/src/main/java/com/digitalinovationone/springbootconfig/config/DBConfiguration.java
@@ -0,0 +1,38 @@
+package com.digitalinovationone.springbootconfig.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasoucer")
+@Getter
+@Setter
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection(){
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
+    @Profile("prod")
+    @Bean
+    public String productionDatabaseConnection(){
+        System.out.println("DB connection for Production - MYSQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_PROD - Production instance";
+
+    }
+}
